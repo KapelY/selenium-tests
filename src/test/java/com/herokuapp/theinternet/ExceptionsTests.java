193bf95e1626744e9112348c2d82d3408ee6d5ef
IKapeliushnyi <zig.tos@gmail.com>
2024-07-08 02:50:33 +0300

    add ExceptionsTests

--- src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
+++ src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
@@ -15,6 +15,7 @@ import java.time.Duration;
 
 public class ExceptionsTests {
     private WebDriver driver;
+
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
@@ -34,7 +35,25 @@ public class ExceptionsTests {
         WebElement row2Input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='row2']/input")));
 
         Assert.assertTrue(row2Input.isDisplayed(), "row2 input is not displayed");
-        row2Input.sendKeys("");
+    }
+
+    @Test
+    public void elementNotInteractableExceptionTest() {
+
+        WebElement addButtonElement = driver.findElement(By.id("add_btn"));
+        addButtonElement.click();
+
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement row2Input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='row2']/input")));
+
+        row2Input.sendKeys("Sushi");
+
+        WebElement save = driver.findElement(By.xpath("//div[@id='row2']/button[@name='Save']"));
+        save.click();
+
+        WebElement confirmation = driver.findElement(By.id("confirmation"));
+        Assert.assertEquals(confirmation.getText(), "Row 2 was saved", "confirmation is not as expected");
     }
 
     @AfterMethod
