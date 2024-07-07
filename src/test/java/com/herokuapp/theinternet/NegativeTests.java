06bd214804f8ebbb8bd6f9db5e9002494e707d37
IKapeliushnyi <zig.tos@gmail.com>
2024-07-08 00:22:41 +0300

    add NegativeTests

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 public class NegativeTests {
 
     @Test
-    public void testNegative() {
+    public void incorrectUserNameTest() {
 
         WebDriver driver = new ChromeDriver();
 
@@ -31,6 +31,32 @@ public class NegativeTests {
 
         errorMessage = driver.findElement(By.xpath("//div[@id='flash']"));
         Assert.assertTrue(errorMessage.getText().contains("Your username is invalid!"), "Message is not as expected");
+
+        driver.quit();
+    }
+
+    @Test
+    public void incorrectPasswordTest() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.manage().window().maximize();
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("incorrect");
+
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.cssSelector("#flash"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed");
+
+        errorMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+        Assert.assertTrue(errorMessage.getText().contains("Your password is invalid!"), "Message is not as expected");
 
         driver.quit();
     }
